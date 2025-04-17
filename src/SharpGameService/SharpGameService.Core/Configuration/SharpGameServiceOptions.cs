d5bd09b581c9db6edc5c1c47713b031c33aaba3c
Luke Parker <lparkermg@outlook.com>
2025-04-17 14:03:34 +0100

    Creates initial project.

---
+++ src/SharpGameService/SharpGameService.Core/Configuration/SharpGameServiceOptions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpGameService.Core.Configuration
+{
+    public class SharpGameServiceOptions
+    {
+        public bool SinglePlayer { get; set; } = true;
+
+        public int MaxPlayersPerRoom { get; set; } = 1;
+
+        public int MaxRooms { get; set; } = 1;
+    }
+}
---
+++ src/SharpGameService/SharpGameService.Extensions/ServiceExtensions.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Hosting;
+using SharpGameService.Core.Configuration;
+
+namespace SharpGameService.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static IServiceCollection AddSharpGameService<TImplementationType>(this IServiceCollection services, Action<SharpGameServiceOptions> options) where TImplementationType : BackgroundService, IHostedService
+        {
+            services.Configure<SharpGameServiceOptions>(options);
+
+            services.AddHostedService<TImplementationType>();
+            return services;
+        }
+
+        public static IApplicationBuilder UseSharpGameService(this IApplicationBuilder host, WebSocketOptions socketOptions)
+        {
+            // Setup any settings for the service.
+
+            // We should be using controllers, but they would likely be mapped in the application rather than the package.
+
+            // Setup websockets
+            host.UseWebSockets(socketOptions);
+            return host;
+        }
+    }
+}
