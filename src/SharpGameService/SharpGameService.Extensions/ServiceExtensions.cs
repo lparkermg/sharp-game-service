02ef55eb5a59e1dac169019593a4e87039509abc
Luke Parker <lparkermg@outlook.com>
2025-04-20 14:54:14 +0100

    Updates service extensions with singleton.

--- src/SharpGameService/SharpGameService.Extensions/ServiceExtensions.cs
+++ src/SharpGameService/SharpGameService.Extensions/ServiceExtensions.cs
@@ -2,16 +2,19 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Hosting;
 using SharpGameService.Core.Configuration;
+using SharpGameService.Core;
+using SharpGameService.Core.Interfaces;
 
 namespace SharpGameService.Extensions
 {
     public static class ServiceExtensions
     {
-        public static IServiceCollection AddSharpGameService<TImplementationType>(this IServiceCollection services, Action<SharpGameServiceOptions> options) where TImplementationType : BackgroundService, IHostedService
+        public static IServiceCollection AddSharpGameService<TImplementationType, TRoomType>(this IServiceCollection services, Action<SharpGameServiceOptions> options) where TImplementationType : BackgroundService, IHostedService where TRoomType : BaseRoom, new()
         {
             services.Configure<SharpGameServiceOptions>(options);
 
             services.AddHostedService<TImplementationType>();
+            services.AddSingleton<IHouse, House<TRoomType>>();
             return services;
         }
 
