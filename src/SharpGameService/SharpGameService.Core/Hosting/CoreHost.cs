f65aa0abe7036d83127131f248ccddfc3c8c1f03
Luke Parker <lparkermg@outlook.com>
2025-04-22 19:01:24 +0100

    Implements a CoreHost for in the core library. Better splits up the library options.

---
+++ src/SharpGameService/SharpGameService.Core/Hosting/CoreHost.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using SharpGameService.Core.Configuration;
+using SharpGameService.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpGameService.Core.Hosting
+{
+    public abstract class CoreHost(ILogger<CoreHost> logger, IOptions<SharpGameServiceOptions> options, IHouse house) : BackgroundService
+    {
+        private readonly SharpGameServiceOptions _options = options.Value;
+
+        public override Task StartAsync(CancellationToken cancellationToken)
+        {
+            if (_options.House.SinglePlayer)
+            {
+                house.CreateRoom(_options.Rooms.SinglePlayerRoomId, _options.Rooms.SinglePlayerRoomCode);
+                logger.LogInformation("Service set to single player mode, room created with id of singleplayer and no room code.");
+            }
+            logger.LogInformation("Started core game service.");
+            return base.StartAsync(cancellationToken);
+        }
+
+        public override Task StopAsync(CancellationToken cancellationToken)
+        {
+            // TODO: Implement house cleanup logic (closing connections etc)
+            return base.StopAsync(cancellationToken);
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                await house.ProcessAsync();
+                await Task.Delay(1000 / _options.House.TicksPerSecond, stoppingToken);
+                await Task.Yield();
+            }
+        }
+    }
+}
